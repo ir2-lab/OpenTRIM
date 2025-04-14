1b3c1be260342a46dc0609e6c032e8416a8589dc
George Apostolopoulos <gapost@ipta.demokritos.gr>
2025-04-14 10:28:40 +0300

    Automatic generation of H5 output documentation

---
+++ doc/h5file.dox.md
@@ -0,0 +1,340 @@
+
+## The HDF5 output archive {#out_file}
+
+The simulation produces a single HDF5 output file which contains all
+results from tallies and events along with the input conditions, run
+timing and other information. 
+ 
+A brief description of the file contents is given here. More detailed
+information can be found within the file itself. 
+
+Dimensions of tables depend on:
+- \f$N_{at}\f$ : # of atoms, i.e., all atoms in the target plus the
+projectile. Note that the same atom species belonging to different
+materials is counted as different atom.
+- \f$N_{mat}\f$ : # of target materials
+- \f$N_x, N_y, N_z\f$ : # of grid points along the 3 axes
+- \f$N_c=(N_x-1)(N_y-1)(N_z-1)\f$ : # of target cells
+- \f$N_e\f$ : # of energy points for energy loss tables
+- \f$N_{ev}\f$ : # of events
+
+<table>
+<caption>HDF5 output archive structure</caption>
+<tr><th>Path
+<th>Type
+<th>Size
+<th>Description
+<tr><td>/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;run_info/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;title
+<td>String
+<td>Scalar
+<td>User supplied simulation title
+<tr><td>&emsp;&emsp;&emsp;&emsp;total_ion_count
+<td>Numeric
+<td>Scalar
+<td>Total number of simulated ions
+<tr><td>&emsp;&emsp;&emsp;&emsp;config_json
+<td>String
+<td>Scalar
+<td>JSON formatted simulation options
+<tr><td>&emsp;&emsp;&emsp;&emsp;variable_list
+<td>String
+<td>Scalar
+<td>List of all data variables in the file
+<tr><td>&emsp;&emsp;&emsp;&emsp;version_info/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;name
+<td>String
+<td>Scalar
+<td>program name
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;version
+<td>String
+<td>Scalar
+<td>version
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;compiler
+<td>String
+<td>Scalar
+<td>compiler
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;compiler_version
+<td>String
+<td>Scalar
+<td>compiler version
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;build_system
+<td>String
+<td>Scalar
+<td>build system
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;build_time
+<td>String
+<td>Scalar
+<td>build time
+<tr><td>&emsp;&emsp;&emsp;&emsp;run_history
+<td>String
+<td>Scalar
+<td>run history
+<tr><td>&emsp;&emsp;&emsp;&emsp;rng_state
+<td>Numeric
+<td>4
+<td>random generator state
+<tr><td>&emsp;&emsp;target/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;grid/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;name
+<td>String
+<td>Scalar
+<td>program name
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;X
+<td>Numeric
+<td>Nx
+<td>x-axis grid
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Y
+<td>Numeric
+<td>Nx
+<td>y-axis grid
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Z
+<td>Numeric
+<td>Nx
+<td>z-axis grid
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;cell_xyz
+<td>Numeric
+<td>Nc x 3
+<td>Cell center coordinates
+<tr><td>&emsp;&emsp;&emsp;&emsp;atoms/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;label
+<td>String
+<td>Nat
+<td>labels = [Atom (Chemical symbol)] in [Material]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;symbol
+<td>String
+<td>Nat
+<td>Chemical symbol
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Z
+<td>Numeric
+<td>Nat
+<td>Atomic number
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;M
+<td>Numeric
+<td>Nat
+<td>Atomic mass
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Ed
+<td>Numeric
+<td>Nat
+<td>Displacement energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;El
+<td>Numeric
+<td>Nat
+<td>Lattice binding energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Es
+<td>Numeric
+<td>Nat
+<td>Surface binding energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Er
+<td>Numeric
+<td>Nat
+<td>Replacement energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;materials/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;name
+<td>String
+<td>Nmat
+<td>name of material
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;atomic_density
+<td>Numeric
+<td>Nmat
+<td>atomic density [at/nm^3]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;mass_density
+<td>Numeric
+<td>Nmat
+<td>mass density [at/nm^3]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;atomic_radius
+<td>Numeric
+<td>Nmat
+<td>atomic radius [nm]
+<tr><td>&emsp;&emsp;&emsp;&emsp;dedx/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;erg
+<td>Numeric
+<td>Ne
+<td>dEdx table energy grid [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;eloss
+<td>Numeric
+<td>Nat x Nmat x Ne
+<td>dEdx values [eV/nm]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;strag
+<td>Numeric
+<td>Nat x Nmat x Ne
+<td>straggling values [eV/nm]
+<tr><td>&emsp;&emsp;&emsp;&emsp;flight_path/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;erg
+<td>Numeric
+<td>Ne
+<td>flight path table energy grid [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;mfp
+<td>Numeric
+<td>Nat x Nmat x Ne
+<td>mean free path [nm]
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Tcutoff
+<td>Numeric
+<td>Nat x Nmat x Ne
+<td>recoil energy cut0ff [eV]
+<tr><td>&emsp;&emsp;ion_beam/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;E0
+<td>Numeric
+<td>Scalar
+<td>mean energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;Z
+<td>Numeric
+<td>Scalar
+<td>atomic number
+<tr><td>&emsp;&emsp;&emsp;&emsp;M
+<td>Numeric
+<td>Scalar
+<td>mass [amu]
+<tr><td>&emsp;&emsp;&emsp;&emsp;dir0
+<td>Numeric
+<td>3
+<td>mean direction
+<tr><td>&emsp;&emsp;&emsp;&emsp;x0
+<td>Numeric
+<td>3
+<td>mean position [nm]
+<tr><td>&emsp;&emsp;tally/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;damage_events/Vacancies
+<td>Numeric
+<td>Nat x Nc
+<td>Vacancies
+<tr><td>&emsp;&emsp;&emsp;&emsp;damage_events/Implantations
+<td>Numeric
+<td>Nat x Nc
+<td>Implantations & Interstitials
+<tr><td>&emsp;&emsp;&emsp;&emsp;damage_events/Replacements
+<td>Numeric
+<td>Nat x Nc
+<td>Replacements
+<tr><td>&emsp;&emsp;&emsp;&emsp;damage_events/Recombinations
+<td>Numeric
+<td>Nat x Nc
+<td>Intra-cascade recombinations
+<tr><td>&emsp;&emsp;&emsp;&emsp;damage_events/Displacements
+<td>Numeric
+<td>Nat x Nc
+<td>Displacements
+<tr><td>&emsp;&emsp;&emsp;&emsp;energy_deposition/Ionization
+<td>Numeric
+<td>Nat x Nc
+<td>Energy deposited to ionization [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;energy_deposition/Lattice
+<td>Numeric
+<td>Nat x Nc
+<td>Energy deposited to the lattice as thermal energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;energy_deposition/Stored
+<td>Numeric
+<td>Nat x Nc
+<td>Energy stored in lattice defects [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;energy_deposition/Lost
+<td>Numeric
+<td>Nat x Nc
+<td>Energy lost due to ions exiting the simulation [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka_damage/Pka
+<td>Numeric
+<td>Nat x Nc
+<td>Primary knock-on atoms (PKAs)
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka_damage/Pka_energy
+<td>Numeric
+<td>Nat x Nc
+<td>PKA recoil energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka_damage/Tdam
+<td>Numeric
+<td>Nat x Nc
+<td>Damage energy [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka_damage/Tdam_LSS
+<td>Numeric
+<td>Nat x Nc
+<td>Damage energy estimated by the LSS approximation [eV]
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka_damage/Vnrt
+<td>Numeric
+<td>Nat x Nc
+<td>Vacancies per the NRT model using Tdam
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka_damage/Vnrt_LSS
+<td>Numeric
+<td>Nat x Nc
+<td>Vacancies per the NRT model using Tdam_LSS
+<tr><td>&emsp;&emsp;&emsp;&emsp;ion_stat/Flight_path
+<td>Numeric
+<td>Nat x Nc
+<td>Flight path [nm]
+<tr><td>&emsp;&emsp;&emsp;&emsp;ion_stat/Collisions
+<td>Numeric
+<td>Nat x Nc
+<td>Ion collisions
+<tr><td>&emsp;&emsp;&emsp;&emsp;ion_stat/Lost
+<td>Numeric
+<td>Nat x Nc
+<td>Ions that exit the simulation volume
+<tr><td>&emsp;&emsp;&emsp;&emsp;totals/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;data
+<td>Numeric
+<td>19 x Nat
+<td>tally totals per atom
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;column_names
+<td>String
+<td>19
+<td>names of totals
+<tr><td>&emsp;&emsp;events/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;pka/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;event_data
+<td>Numeric
+<td>Nev x 10
+<td>event data
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;column_names
+<td>String
+<td>10
+<td>event data column names
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;column_descriptions
+<td>String
+<td>10
+<td>event data column descriptions
+<tr><td>&emsp;&emsp;&emsp;&emsp;exit/
+<td>Group<td><td>
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;event_data
+<td>Numeric
+<td>Nev x 10
+<td>event data
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;column_names
+<td>String
+<td>10
+<td>event data column names
+<tr><td>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;column_descriptions
+<td>String
+<td>10
+<td>event data column descriptions
+</table>
+
+To reach a variable in the archive use the complete path, e.g. `/tally/damage/Tdam`.
+
+Each number in the tally tables is a mean value over all histories.
+For each tally table the standard error of the mean (SEM) is also included. This is a separate table of equal dimension and with the same name plus the ending `_sem`. E.g., for the table `/tally/damage/Tdam` there is also `/tally/damage/Tdam_sem`.
+
+The definition of SEM employed is as follows: if \f$x_i\f$ is the contribution to the table entry \f$x\f$ from the \f$i\f$-th ion history, then the mean, \f$\bar{x}\f$, and the SEM, \f$\f$\sigma_{\bar{x}}\f$\f$, listed in the output file are calculated as:
+$$
+\bar{x} = \frac{1}{N_h} \sum_i {x_i}
+$$
+$$
+\sigma_{\bar{x}}^2 = \frac{1}{N_h(N_h-1)} \sum_i { (x_i - \bar{x})^2 }=
+\frac{\bar{{x^2}} - \bar{x}^2}{N_h-1}
+$$
+
+\see tally
+
+
+
--- source/src/genoptionsdoc.cpp
+++ source/src/genoptionsdoc.cpp
@@ -35,22 +35,63 @@ where `config.json` is a file containing the JSON configuration.
 
 In a C++ program one can use the \ref mcdriver::options class which offers
 a parseJSON() function to parse and validate the options.
+                        
+The easiest way to get started is to generate a template with all default options by running
+                                        
+    opentrim -t > template.json
+                        
+and make changes to the new template file.
 
-The JSON-formatted options string has the following self-explanatory structure shown bellow.
+@note `opentrim` accepts comments in JSON 
+
+The JSON config string has the following self-explanatory structure shown bellow.
 Click on any of the options to see more information.
 
 )";
 
 const char *postJSON = R"(
-@note `opentrim` accepts comments in the JSON config string
-                        
-The easiest way to get started is to get a template with all default options by running
-                                        
-    opentrim -t > template.json
-                        
-and make changes to the new template file.
-                
-Most of the options shown above are default values and can be omitted.
+
+)";
+
+const char *preampleH5 = R"(
+## The HDF5 output archive {#out_file}
+
+The simulation produces a single HDF5 output file which contains all
+results from tallies and events along with the input conditions, run
+timing and other information. 
+ 
+A brief description of the file contents is given here. More detailed
+information can be found within the file itself. 
+
+Dimensions of tables depend on:
+- \f$N_{at}\f$ : # of atoms, i.e., all atoms in the target plus the
+projectile. Note that the same atom species belonging to different
+materials is counted as different atom.
+- \f$N_{mat}\f$ : # of target materials
+- \f$N_x, N_y, N_z\f$ : # of grid points along the 3 axes
+- \f$N_c=(N_x-1)(N_y-1)(N_z-1)\f$ : # of target cells
+- \f$N_e\f$ : # of energy points for energy loss tables
+- \f$N_{ev}\f$ : # of events
+
+)";
+
+const char *postH5 = R"(
+To reach a variable in the archive use the complete path, e.g. `/tally/damage/Tdam`.
+
+Each number in the tally tables is a mean value over all histories.
+For each tally table the standard error of the mean (SEM) is also included. This is a separate table of equal dimension and with the same name plus the ending `_sem`. E.g., for the table `/tally/damage/Tdam` there is also `/tally/damage/Tdam_sem`.
+
+The definition of SEM employed is as follows: if \f$x_i\f$ is the contribution to the table entry \f$x\f$ from the \f$i\f$-th ion history, then the mean, \f$\bar{x}\f$, and the SEM, \f$\f$\sigma_{\bar{x}}\f$\f$, listed in the output file are calculated as:
+$$
+\bar{x} = \frac{1}{N_h} \sum_i {x_i}
+$$
+$$
+\sigma_{\bar{x}}^2 = \frac{1}{N_h(N_h-1)} \sum_i { (x_i - \bar{x})^2 }=
+\frac{\bar{{x^2}} - \bar{x}^2}{N_h-1}
+$$
+
+\see tally
+
 )";
 
 const char *tableStart = "<table>\n"
@@ -58,27 +99,43 @@ const char *tableStart = "<table>\n"
 
 const char *tableEnd = "</table>\n";
 
+void printH5table(std::ostream &os);
+
 int main(int argc, char *argv[])
 {
-    cout << "[genoptionsdoc] input file: " << argv[1] << endl;
-    std::ifstream is(argv[1]);
-    ojson j = ojson::parse(is, nullptr, true, true);
+    {
+        cout << "[genoptionsdoc] input file: " << argv[1] << endl;
+        std::ifstream is(argv[1]);
+        ojson j = ojson::parse(is, nullptr, true, true);
 
-    std::ofstream os("options.dox.md");
+        std::ofstream os("options.dox.md");
 
-    os << preampleJSON;
-    os << "> <br>" << endl;
-    linkID = 0;
-    jsonPrint(os, j);
-    os << "<br>" << endl << endl;
+        os << preampleJSON;
+        os << "> <br>" << endl;
+        linkID = 0;
+        jsonPrint(os, j);
+        os << "<br>" << endl << endl;
 
-    os << postJSON;
+        os << postJSON;
 
-    os << endl << endl;
-    os << tableStart;
-    linkID = 0;
-    jsonPrintTable(os, j);
-    os << tableEnd;
+        os << endl << endl;
+        os << tableStart;
+        linkID = 0;
+        jsonPrintTable(os, j);
+        os << tableEnd;
+    }
+
+    {
+        std::ofstream os("h5file.dox.md");
+
+        os << preampleH5;
+
+        printH5table(os);
+
+        os << postH5;
+
+        os << endl << endl;
+    }
 
     return 0;
 }
@@ -305,4 +362,208 @@ void jsonPrintTable(std::ostream &os, const ojson &j, type_t type, string path)
                 os << *it << "<br>" << endl;
         }
     }
+}
+
+enum ds_type { ds_group, ds_string, ds_numeric };
+
+struct dsd;
+
+typedef std::vector<dsd> ds_list_t;
+
+struct dsd
+{
+    string name;
+    ds_type type{ ds_group };
+    std::vector<string> shape;
+    string description;
+    ds_list_t children{};
+    void add(const dsd &d) { children.push_back(d); }
+};
+
+int create_h5_dir(dsd &root)
+{
+    {
+        // 1. run_info
+        dsd run_info{ "run_info" };
+        run_info.add({ "title", ds_string, { "Scalar" }, "User supplied simulation title" });
+        run_info.add(
+                { "total_ion_count", ds_numeric, { "Scalar" }, "Total number of simulated ions" });
+        run_info.add(
+                { "config_json", ds_string, { "Scalar" }, "JSON formatted simulation options" });
+        run_info.add({ "variable_list",
+                       ds_string,
+                       { "Scalar" },
+                       "List of all data variables in the file" });
+        {
+            dsd version_info{ "version_info" };
+            version_info.add({ "name", ds_string, { "Scalar" }, "program name" });
+            version_info.add({ "version", ds_string, { "Scalar" }, "version" });
+            version_info.add({ "compiler", ds_string, { "Scalar" }, "compiler" });
+            version_info.add({ "compiler_version", ds_string, { "Scalar" }, "compiler version" });
+            version_info.add({ "build_system", ds_string, { "Scalar" }, "build system" });
+            version_info.add({ "build_time", ds_string, { "Scalar" }, "build time" });
+            run_info.add(version_info);
+        }
+        run_info.add({ "run_history", ds_string, { "Scalar" }, "run history" });
+        run_info.add({ "rng_state", ds_numeric, { "4" }, "random generator state" });
+        root.add(run_info);
+    }
+    {
+        // 2. target
+        dsd g{ "target" };
+        {
+            dsd ch{ "grid" };
+            ch.add({ "name", ds_string, { "Scalar" }, "program name" });
+            ch.add({ "X", ds_numeric, { "Nx" }, "x-axis grid" });
+            ch.add({ "Y", ds_numeric, { "Nx" }, "y-axis grid" });
+            ch.add({ "Z", ds_numeric, { "Nx" }, "z-axis grid" });
+            ch.add({ "cell_xyz", ds_numeric, { "Nc", "3" }, "Cell center coordinates" });
+            g.add(ch);
+        }
+        {
+            dsd ch{ "atoms" };
+            ch.add({ "label",
+                     ds_string,
+                     { "Nat" },
+                     "labels = [Atom (Chemical symbol)] in [Material]" });
+            ch.add({ "symbol", ds_string, { "Nat" }, "Chemical symbol" });
+            ch.add({ "Z", ds_numeric, { "Nat" }, "Atomic number" });
+            ch.add({ "M", ds_numeric, { "Nat" }, "Atomic mass" });
+            ch.add({ "Ed", ds_numeric, { "Nat" }, "Displacement energy [eV]" });
+            ch.add({ "El", ds_numeric, { "Nat" }, "Lattice binding energy [eV]" });
+            ch.add({ "Es", ds_numeric, { "Nat" }, "Surface binding energy [eV]" });
+            ch.add({ "Er", ds_numeric, { "Nat" }, "Replacement energy [eV]" });
+            g.add(ch);
+        }
+        {
+            dsd ch{ "materials" };
+            ch.add({ "name", ds_string, { "Nmat" }, "name of material" });
+            ch.add({ "atomic_density", ds_numeric, { "Nmat" }, "atomic density [at/nm^3]" });
+            ch.add({ "mass_density", ds_numeric, { "Nmat" }, "mass density [at/nm^3]" });
+            ch.add({ "atomic_radius", ds_numeric, { "Nmat" }, "atomic radius [nm]" });
+            g.add(ch);
+        }
+        {
+            dsd ch{ "dedx" };
+            ch.add({ "erg", ds_numeric, { "Ne" }, "dEdx table energy grid [eV]" });
+            ch.add({ "eloss", ds_numeric, { "Nat", "Nmat", "Ne" }, "dEdx values [eV/nm]" });
+            ch.add({ "strag", ds_numeric, { "Nat", "Nmat", "Ne" }, "straggling values [eV/nm]" });
+            g.add(ch);
+        }
+        {
+            dsd ch{ "flight_path" };
+            ch.add({ "erg", ds_numeric, { "Ne" }, "flight path table energy grid [eV]" });
+            ch.add({ "mfp", ds_numeric, { "Nat", "Nmat", "Ne" }, "mean free path [nm]" });
+            ch.add({ "Tcutoff", ds_numeric, { "Nat", "Nmat", "Ne" }, "recoil energy cut0ff [eV]" });
+            g.add(ch);
+        }
+        root.add(g);
+    }
+    {
+        // 3. ion beam
+        dsd g{ "ion_beam" };
+        g.add({ "E0", ds_numeric, { "Scalar" }, "mean energy [eV]" });
+        g.add({ "Z", ds_numeric, { "Scalar" }, "atomic number" });
+        g.add({ "M", ds_numeric, { "Scalar" }, "mass [amu]" });
+        g.add({ "dir0", ds_numeric, { "3" }, "mean direction" });
+        g.add({ "x0", ds_numeric, { "3" }, "mean position [nm]" });
+        root.add(g);
+    }
+    {
+        // 4. tally
+        tally t;
+        dsd g{ "tally" };
+        int k = 1;
+        while (k < tally::std_tallies) {
+            std::string name;
+            name += tally::arrayGroup(k);
+            name += "/";
+            name += tally::arrayName(k);
+            g.add({ name, ds_numeric, { "Nat", "Nc" }, tally::arrayDescription(k) });
+            k++;
+        }
+        {
+            dsd ch{ "totals" };
+            ch.add({ "data",
+                     ds_numeric,
+                     { std::to_string(tally::std_tallies), "Nat" },
+                     "tally totals per atom" });
+            ch.add({ "column_names",
+                     ds_string,
+                     { std::to_string(tally::std_tallies) },
+                     "names of totals" });
+            g.add(ch);
+        }
+        root.add(g);
+    }
+    {
+        // 5. events
+        dsd g{ "events" };
+        {
+            dsd ch{ "pka" };
+            ch.add({ "event_data", ds_numeric, { "Nev", "10" }, "event data" });
+            ch.add({ "column_names", ds_string, { "10" }, "event data column names" });
+            ch.add({ "column_descriptions",
+                     ds_string,
+                     { "10" },
+                     "event data column descriptions" });
+            g.add(ch);
+        }
+        {
+            dsd ch{ "exit" };
+            ch.add({ "event_data", ds_numeric, { "Nev", "10" }, "event data" });
+            ch.add({ "column_names", ds_string, { "10" }, "event data column names" });
+            ch.add({ "column_descriptions",
+                     ds_string,
+                     { "10" },
+                     "event data column descriptions" });
+            g.add(ch);
+        }
+        root.add(g);
+    }
+    return 0;
+}
+
+void h5Print(std::ostream &os, const dsd &d, int level = 0)
+{
+    switch (d.type) {
+    case ds_group:
+        os << "<tr><td>";
+        if (level) {
+            indent(os, level);
+            os << d.name << "/" << endl;
+        } else
+            os << "/" << endl;
+        os << "<td>Group<td><td>" << endl;
+        for (const dsd &d1 : d.children)
+            h5Print(os, d1, level + 1);
+        break;
+    default:
+        os << "<tr><td>";
+        if (level) {
+            indent(os, level);
+        };
+        os << d.name << endl;
+        os << "<td>" << (d.type == ds_string ? "String" : "Numeric") << endl;
+        os << "<td>" << d.shape[0];
+        for (int i = 1; i < d.shape.size(); i++) {
+            os << " x ";
+            os << d.shape[i];
+        }
+        os << endl;
+        os << "<td>" << d.description << endl;
+    }
+}
+
+void printH5table(std::ostream &os)
+{
+    dsd root({ "/" });
+    create_h5_dir(root);
+
+    os << "<table>\n"
+          "<caption>HDF5 output archive structure</caption>\n";
+
+    os << "<tr><th>Path\n<th>Type\n<th>Size\n<th>Description\n";
+    h5Print(os, root);
+    os << "</table>\n";
 }
