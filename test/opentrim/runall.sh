51a581980e111b3ae009907ec519e7f0fc6b4c64
George Apostolopoulos <gapost@ipta.demokritos.gr>
2025-06-13 13:24:46 +0300

    Reorganize test/opentrim
    Put all benchmark json files in the top folder - instead of test/opentrim/b1/config.json have test/opentrim/b1.json

---
+++ test/opentrim/runall.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+opentrim -f b1.json
+opentrim -f b2.json
+opentrim -f b3.json
+opentrim -f b4.json
+opentrim -f b5.json
+opentrim -f b6.json
+opentrim -f b7.json
+opentrim -f b8.json
+
+
+
+
