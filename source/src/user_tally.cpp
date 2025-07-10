e4c497d0a56d90ff7c6a87c761b141f5b1d81991
Iridanos Loulas <eridanous@hotmail.com>
2025-07-10 12:43:25 +0300

    multi-dimensional binning V1

--- source/src/user_tally.cpp
+++ source/src/user_tally.cpp
@@ -4,53 +4,99 @@
 
 void user_tally::init()
 {
-    data_ = ArrayNDd(nbins);
-    bins.resize(nbins, 0);
-    for (int i=0;i<nbins;i++)bins[i]=i*(xmax-xmin)/nbins;
-    auto i = bins.begin();
-    for (; i != bins.end(); i++)
-        *i = (i - bins.begin()) * (xmax - xmin) / nbins;
-    bins[0] = 0;
-    bins[1] = 1;
-    bins[2] = 2;
-    bins[3] = 3;
-    bins[4]= 3.5;
-    bins[5] = 4;
-    bins[6] = 5;
-    bins[7] = 30;
+    if (par_.x.size()){
+        bin_codes.push_back(cX);
+        bins.push_back(par_.x);
+        bin_sizes.push_back(par_.x.size());
+    }
+    if (par_.y.size()){
+        bin_codes.push_back(cY);
+        bins.push_back(par_.y);
+        bin_sizes.push_back(par_.y.size());
+    }
+    if (par_.z.size()){
+        bin_codes.push_back(cZ);
+        bins.push_back(par_.z);
+        bin_sizes.push_back(par_.z.size());
+    }
+    data_ = ArrayNDd(bin_sizes);
 }
 
-// void user_tally::clear()
-// {
-//     data_.clear();
-//     // std::fill(bins.begin(), bins.end(), 0);
-//     // auto test1b=test1;
-// }
-
-int user_tally::get_bin(double x) const
+std::vector<size_t> user_tally::get_bin(const ion &i) const
 {
-    auto binid= std::upper_bound(bins.begin(), bins.end(), x) - bins.begin() - 1;
-    // auto bin_bound = std::upper_bound(bins.begin(), bins.end(), x);
-    // auto binit = std::find(bins.begin(), bins.end(), *bin_bound); //bin bound iterator
-    // int binid = std::distance(bins.begin(), binit) - 1; // id of bin starting from 0
-    return binid;
+    int n = bin_codes.size();
+    std::vector<size_t> idx(n); // same size as bin_sizes
+
+    for (int j=0;j<n;++j){
+        double pos = 0;
+        switch (bin_codes[j]) {
+        case cX:
+            pos = i.pos()[0]; // ion x-position
+            idx[j] = std::upper_bound(par_.x.begin(), par_.x.end(), pos) - par_.x.begin() - 1; // id of bin starting from 0
+            break;
+        case cY:
+            pos = i.pos()[1]; // ion y-position
+            idx[j] = std::upper_bound(par_.y.begin(), par_.y.end(), pos) - par_.y.begin() - 1; // id of bin starting from 0
+            break;
+        case cZ:
+            pos = i.pos()[2]; // ion z-position
+            idx[j] = std::upper_bound(par_.z.begin(), par_.z.end(), pos) - par_.z.begin() - 1; // id of bin starting from 0
+            break;
+        default:
+            break;
+        }
+    }
+    return idx;
 }
+
+// int user_tally::get_bin_old(double xion) const
+// {
+//     auto binid = std::upper_bound(x.begin(), x.end(), xion) - x.begin() - 1;
+//     // auto bin_bound = std::upper_bound(x.begin(), x.end(), xion);
+//     // auto binit = std::find(x.begin(), x.end(), *bin_bound); //bin bound iterator
+//     // int binid = std::distance(x.begin(), binit) - 1; // id of bin starting from 0
+//     return binid;
+// }
 
 void user_tally::operator()(Event ev, const ion &i, const void *pv)
 {
     if (ev != Event::IonStop)
         return;
+    auto idx = get_bin(i);
 
-    double x = i.pos()[0]; // ion x-position
-    int bin = get_bin(x);
 
-    if (bin >= 0) {
-        data_(bin) += 1;
-    }
+    data_(idx)++;
+
 }
 
-void user_tally::print() const
-{
-    for (int i = 0; i < nbins; ++i)
-        std::cout << "Bin " << i << ": " << bins[i] << "\n";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
