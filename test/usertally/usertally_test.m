f9040e0e195787d151e62877d09e6f9cc4080071
George Apostolopoulos <gapost@ipta.demokritos.gr>
2025-10-20 11:24:14 +0300

    Update user_tally test
    - array of tallies
    - save and continue

--- test/usertally/usertally_test.m
+++ test/usertally/usertally_test.m
@@ -3,54 +3,46 @@ clear
 # Load HDF5 Results
 pkg load hdf5oct
 
+file_name = 'usertally_test.h5';
+user_tally_id = {'Implanted Fe', 'Fe Interstitials'};
 
+for iut = 1:2,
 
-D = h5load('usertally_test.h5','/user_tally/Test tally/data');
+  path = ['/user_tally/' user_tally_id{iut}];
+  D = h5load(file_name,[path '/data']);
+  id = h5load(file_name,[path '/data']);
 
-## Due to octave fortran style arrays, we have to transpose the N-D data array
-n = ndims(D);
-D = permute(D,n:-1:1);
+  ## Due to octave fortran style arrays, we have to transpose the N-D data array
+  n = ndims(D);
+  D = permute(D,n:-1:1);
 
-labels = h5load('usertally_test.h5','/user_tally/Test tally/bin_names');
-labels = h5load('usertally_test.h5','/user_tally/Test tally/bin_decriptions');
+  labels = h5load(file_name,[path '/bin_names']);
+  labels = h5load(file_name,[path '/bin_decriptions']);
 
-bins = {};
+  bins = {};
 
-for i=1:n,
-  bins{i} = h5load('usertally_test.h5',['/user_tally/Test tally/bins/' num2str(i-1)]);
+  for i=1:length(labels),
+    bins{i} = h5load(file_name,[path '/bins/' num2str(i-1)]);
+  end
+
+  figure(iut)
+  clf
+  x = bins{2};
+  y = bins{3};
+  x = 0.5*(x(1:end-1) + x(2:end));
+  y = 0.5*(y(1:end-1) + y(2:end));
+  [c,h]=contour(y,x,D);
+  title(["500 keV Fe in Fe, " user_tally_id{iut}])
+  # clabel(c,h)
+  xlabel(labels{3})
+  ylabel(labels{2})
+  axis equal
+  colorbar('location','southoutside')
+
+  print('-dpng',['usertally_test_fig' num2str(iut)]);
+
 end
 
-figure 1
-clf
-d = squeeze(D(1,:,:));
-x = bins{2};
-y = bins{3};
-x = 0.5*(x(1:end-1) + x(2:end));
-y = 0.5*(y(1:end-1) + y(2:end));
-[c,h]=contour(y,x,d);
-title("500 keV Fe in Fe, Beam ions stopping in the material")
-# clabel(c,h)
-xlabel(labels{3})
-ylabel(labels{2})
-axis equal
-colorbar('location','southoutside')
-print -dpng usertally_test_fig1
-
-figure 2
-clf
-d = squeeze(D(2,:,:));
-x = bins{2};
-y = bins{3};
-x = 0.5*(x(1:end-1) + x(2:end));
-y = 0.5*(y(1:end-1) + y(2:end));
-[c,h]=contour(y,x,d);
-title("500 keV Fe in Fe, Recoils stopping in the material")
-# clabel(c,h)
-xlabel(labels{3})
-ylabel(labels{2})
-axis equal
-colorbar('location','southoutside')
-print -dpng usertally_test_fig2
 
 
 
